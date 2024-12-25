15aa3000c1abaffc79c21dcff9b48035a65e62ca
nagaraj-kr <k.r.nagaraj02@gmail.com>
2024-12-25 23:54:35 +0530

    Update main.py
    changing the host as 0000 make it global access

--- main.py
+++ main.py
@@ -157,4 +157,4 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
